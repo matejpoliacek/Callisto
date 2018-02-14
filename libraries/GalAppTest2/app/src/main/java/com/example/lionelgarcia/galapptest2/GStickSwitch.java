89debe4130dd5d1c8c0f5798fdd17afcdc87bcab
Matej Poliacek <Matej Poliacek@esa-ad.esa.int>
2018-02-14 16:01:42 +0100

    first merge of lionel's and matej's work into a wrapper project

---
+++ libraries/GalAppTest2/app/src/main/java/com/example/lionelgarcia/galapptest2/GStickSwitch.java
@@ -0,0 +1,117 @@
+package com.example.lionelgarcia.galapptest2;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by lgr on 20/01/2018.
+ */
+
+public class GStickSwitch extends LinearLayout {
+
+    private Boolean mswitched;
+    private ImageView mbutton;
+    private TextView mLabel;
+    private View mView;
+    private Context mContext;
+
+    private String mLabelText;
+
+    private Bitmap mbitmap_on;
+    private Bitmap mbitmap_off;
+
+    public GStickSwitch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mContext = context;
+
+        mbitmap_on = BitmapFactory.decodeResource(getResources(), R.drawable.on_switch);
+        mbitmap_off = BitmapFactory.decodeResource(getResources(), R.drawable.off_switch);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.ButtonSwitch,
+                0, 0);
+
+        try {
+            mLabelText = a.getString(R.styleable.ButtonSwitch_label);
+        } finally {
+            a.recycle();
+        }
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.stick_switch, this, true);
+
+        mView = this.findViewById(R.id.main_layout);
+
+        mView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                changeState();
+            }
+        });
+
+        mbutton = mView.findViewById(R.id.button);
+        mLabel = mView.findViewById(R.id.label);
+
+        mLabel.setText(mLabelText);
+
+        mswitched = false;
+
+        SetState(mswitched);
+    }
+
+    public GStickSwitch(Context context) {
+        this(context, null);
+    }
+
+    public void changeState(){
+
+        mswitched = !mswitched;
+        SetState(mswitched);
+
+    }
+
+    public void SetState(Boolean switched){
+
+        mswitched = switched;
+
+        if(switched){
+            mbutton.setImageBitmap(mbitmap_on);
+            mLabel.setTextColor(ContextCompat.getColor(mContext, R.color.label_on));
+        }
+        else{
+            mbutton.setImageBitmap(mbitmap_off);
+            mLabel.setTextColor(ContextCompat.getColor(mContext, R.color.label_off));
+        }
+    }
+
+    public Boolean getState(){
+        return mswitched;
+    }
+
+    public void setOnClickListener(View.OnClickListener view_listener){
+        mView.setOnClickListener(view_listener);
+    }
+
+    public void setEnabled(Boolean enabled) {
+        if(enabled){
+            mView.setVisibility(View.VISIBLE);
+        }
+        else{
+            mView.setVisibility(View.INVISIBLE);
+        }
+    }
+}
---
+++ libraries/GalAppTest2/app/src/main/java/com/example/lionelgarcia/galapptest2/SatelliteItemAdapter.java
@@ -0,0 +1,109 @@
+package com.example.lionelgarcia.galapptest2;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by lgr on 06/01/2018.
+ */
+
+public class SatelliteItemAdapter extends RecyclerView.Adapter<SatelliteItemAdapter.MyViewHolder> {
+
+    private List<Satellite> mSatellites;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView midView, mconstelationView;
+        private ImageView mflagView;
+        private ImageView msignalView;
+
+        public MyViewHolder(View view) {
+            super(view);
+            midView = view.findViewById(R.id.id);
+            mflagView = view.findViewById(R.id.flag);
+            msignalView = view.findViewById(R.id.signal);
+            mconstelationView = view.findViewById(R.id.constellation);
+        }
+    }
+
+    public SatelliteItemAdapter(List<Satellite> satellites) {
+        this.mSatellites = satellites;
+    }
+
+    @Override
+    public SatelliteItemAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.sattelite_item_row, parent, false);
+
+        return new SatelliteItemAdapter.MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Satellite satellite = mSatellites.get(position);
+        holder.midView.setText(String.format("%05d", satellite.getMid()));
+        holder.mflagView.setImageResource(getFlagBitmap(satellite.getMoperator()));
+        holder.msignalView.setImageResource(getSignalBitmap(satellite.getMsignal()));
+        holder.mconstelationView.setText(satellite.getConstellationName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSatellites.size();
+    }
+
+    /**
+     * Retrieve the flag Bitmap associated to flag_id flag
+     */
+    public int getFlagBitmap(int flag_id){
+
+            switch (flag_id) {
+                case 1:
+                    return R.drawable.us_flag;
+                case 2:
+                    return R.drawable.ru_flag;
+                case 3:
+                    return R.drawable.ru_flag;
+                case 4:
+                    return R.drawable.ja_flag;
+                case 5:
+                    return R.drawable.ch_flag;
+                case 6:
+                    return R.drawable.eu_flag;
+                default:
+                    return R.drawable.unknown_flag;
+            }
+    }
+
+    /**
+      * Retrieve the signal Bitmap associated to signal_id signal level
+      */
+    public int getSignalBitmap(int signal_id){
+
+        switch(signal_id){
+            case 0:
+                return R.drawable.signal_0;
+            case 1:
+                return R.drawable.signal_1;
+            case 2:
+                return R.drawable.signal_2;
+            case 3 :
+                return R.drawable.signal_3;
+            case 4 :
+                return R.drawable.signal_4;
+            case 5 :
+                return R.drawable.signal_5;
+            case 6 :
+                return R.drawable.signal_6;
+            default:
+                return R.drawable.signal_default;
+        }
+
+    }
+
+}
