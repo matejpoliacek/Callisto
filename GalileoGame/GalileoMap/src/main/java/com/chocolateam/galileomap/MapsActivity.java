925494c1357e3cd9a5e5bcc377fb3f8bc036b4f9
Matej Poliacek <poliacek.matej@gmail.com>
2018-04-13 16:58:52 +0200

    basic preparation for tutorial

--- GalileoGame/GalileoMap/src/main/java/com/chocolateam/galileomap/MapsActivity.java
+++ GalileoGame/GalileoMap/src/main/java/com/chocolateam/galileomap/MapsActivity.java
@@ -404,10 +404,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     mLocationPermissionGranted = true;
+                    updateLocationUI();
                 }
             }
         }
-        updateLocationUI();
+
     }
 
 
