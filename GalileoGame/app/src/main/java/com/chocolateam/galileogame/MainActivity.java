2140254c314b821a83bf7dbd5b28ad03b914bc3e
unknown <retepvan@yahoo.co.uk>
2018-05-22 01:29:01 +0200

    fragment doesn't autostart but no crash if permissions granted

--- GalileoGame/app/src/main/java/com/chocolateam/galileogame/MainActivity.java
+++ GalileoGame/app/src/main/java/com/chocolateam/galileogame/MainActivity.java
@@ -1,8 +1,14 @@
 package com.chocolateam.galileogame;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,19 +22,25 @@ import com.chocolateam.galileospaceship.SpaceshipViewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String[] REQUIRED_PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private static final int LOCATION_REQUEST_ID = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        requestPermissionAndSetupFragments(this);
 
         // Start the blank fragment initiating Gal/Gps PVT on app start
-        FragmentManager gamefragmentManager = getSupportFragmentManager();
+        /*FragmentManager gamefragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = gamefragmentManager.beginTransaction();
         BlankFragment pvtFrag = new BlankFragment();
         fragmentTransaction.add(android.R.id.content, pvtFrag).commit();
-        Log.e("uvodny text",String.valueOf(com.chocolateam.galileopvt.BlankFragment.getUserLatitudeDegrees()));
+        Log.e("uvodny text",String.valueOf(com.chocolateam.galileopvt.BlankFragment.getUserLatitudeDegrees()));*/
     }
 
     public void goToGame(View view) {
@@ -51,5 +63,27 @@ public class MainActivity extends AppCompatActivity {
     public void goToPVT(View view) {
         Intent intent = new Intent(this, PvtActivity.class);
         startActivity(intent);
+    }
+
+    private boolean hasPermissions(Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            // Permissions granted at install time.
+            return true;
+        }
+        for (String p : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(activity, p) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void requestPermissionAndSetupFragments(final Activity activity) {
+        if (hasPermissions(activity)) {
+            return;
+        } else {
+
+            ActivityCompat.requestPermissions(activity, REQUIRED_PERMISSIONS, LOCATION_REQUEST_ID);
+        }
     }
 }
