95f5571a82cf0999588a3134028c749337d34600
Matej Poliacek <poliacek.matej@gmail.com>
2018-08-03 13:29:08 +0200

    add description activity and layout files (to be implemented)

---
+++ GalileoGame/app/src/main/java/com/chocolateam/galileogame/DescriptionActivity.java
@@ -0,0 +1,13 @@
+package com.chocolateam.galileogame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+    }
+}
