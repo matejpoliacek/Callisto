0326afb429fffd9220f7de6d3e5386722ebae618
Matej Poliacek <poliacek.matej@gmail.com>
2018-09-17 17:23:28 +0200

    map finished, TODO: harmonise graphics

--- GalileoGame/GalileoMap/src/main/java/com/chocolateam/galileomap/MapsOnlyActivity.java
+++ GalileoGame/GalileoMap/src/main/java/com/chocolateam/galileomap/MapsOnlyActivity.java
@@ -13,6 +13,7 @@ import com.galfins.gnss_compare.CalculationModule;
 import com.galfins.gnss_compare.StartGNSSFragment;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -50,68 +51,25 @@ public class MapsOnlyActivity extends MapsActivity implements OnMapReadyCallback
             final String constName = ((CalculationModule.CalculationModuleObservable) o).getParentReference().getConstellation().getName();
             Log.e("MAP-CONST", constName);
 
-            if (constName.equals("GPS")) {
-                GPSpoint = new LatLng(lat, lng);
-                Log.e("MAP-GPSPoint", GPSpoint.toString());
-            } else if (constName.equals("Galileo")) {
-                GalileoPoint = new LatLng(lat, lng);
-                Log.e("MAP-GALPoint", GalileoPoint.toString());
-            } else if (constName.equals("Galileo + GPS")) {
-                GalGPSPoint = new LatLng(lat, lng);
-                Log.e("MAP-GALGPSPoint", GalGPSPoint.toString());
-            }
-            /**
             // SET MAP LOCATION, markers and default
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    mGPSMarker = processMarker(checkBoxGPS.isChecked(), mGPSMarker, GPSpoint, BitmapDescriptorFactory.HUE_RED);
-                    mGALMarker = processMarker(checkBoxGAL.isChecked(), mGALMarker, GalileoPoint, BitmapDescriptorFactory.HUE_BLUE);
-                    mGALGPSMarker = processMarker(checkBoxGPS.isChecked(), checkBoxGAL.isChecked(), mGALGPSMarker, GalGPSPoint, BitmapDescriptorFactory.HUE_VIOLET);
+                    if (constName.equals("GPS")) {
+                        GPSpoint = new LatLng(lat, lng);
+                        Log.e("MAP-GPSPoint", GPSpoint.toString());
+                        mGPSMarker = processMarker(checkBoxGPS.isChecked(), mGPSMarker, GPSpoint, BitmapDescriptorFactory.HUE_GREEN);
+                    } else if (constName.equals("Galileo")) {
+                        GalileoPoint = new LatLng(lat, lng);
+                        Log.e("MAP-GALPoint", GalileoPoint.toString());
+                        mGALMarker = processMarker(checkBoxGAL.isChecked(), mGALMarker, GalileoPoint, BitmapDescriptorFactory.HUE_ORANGE);
+                    } else if (constName.equals("Galileo + GPS")) {
+                        GalGPSPoint = new LatLng(lat, lng);
+                        Log.e("MAP-GALGPSPoint", GalGPSPoint.toString());
+                        mGALGPSMarker = processMarker(checkBoxGPS.isChecked(), checkBoxGAL.isChecked(), mGALGPSMarker, GalGPSPoint, BitmapDescriptorFactory.HUE_CYAN);
+                    }
                 }
             });
-            **/
-
-            // replaced by nicer methods -->
-            if (checkBoxGPS.isChecked()){
-                if (mGPSMarker == null) {
-                    mGPSMarker = mMap.addMarker(new MarkerOptions().position(GPSpoint));
-                    mGPSMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                } else {
-                    mGPSMarker.remove();
-                    mGPSMarker = mMap.addMarker(new MarkerOptions().position(GPSpoint));
-                    mGPSMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                }
-            } else if (!checkBoxGPS.isChecked() && mGPSMarker != null) {
-                mGPSMarker.remove();
-            }
-
-            if (checkBoxGAL.isChecked()){
-                if (mGALMarker == null) {
-                    mGALMarker = mMap.addMarker(new MarkerOptions().position(GalileoPoint));
-                    mGALMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-                } else {
-                    mGALMarker.remove();
-                    mGALMarker = mMap.addMarker(new MarkerOptions().position(GalileoPoint));
-                    mGALMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-                }
-            } else if (!checkBoxGAL.isChecked() && mGALMarker != null) {
-                mGALMarker.remove();
-            }
-
-
-            if (checkBoxGAL.isChecked() && checkBoxGPS.isChecked()){
-                if (mGALGPSMarker == null) {
-                    mGALGPSMarker = mMap.addMarker(new MarkerOptions().position(GalGPSPoint));
-                    mGALGPSMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
-                } else {
-                    mGALGPSMarker.remove();
-                    mGALGPSMarker= mMap.addMarker(new MarkerOptions().position(GalGPSPoint));
-                    mGALGPSMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
-                }
-            } else if ((!checkBoxGAL.isChecked() || !checkBoxGPS.isChecked())&& mGALGPSMarker != null) {
-                mGALGPSMarker.remove();
-            }
         }
     };
 
@@ -125,66 +83,6 @@ public class MapsOnlyActivity extends MapsActivity implements OnMapReadyCallback
 
         mapBottomPanel = findViewById(R.id.map_bottom_panel);
         mapBottomPanel.setVisibility(View.VISIBLE);
-
-//       checkBoxGPS = mapBottomPanel.getCheckBoxGPS();
-//       checkBoxGAL = mapBottomPanel.getCheckBoxGAL();
-
-        /** Location Manager **/ /**
-        mLocationListenerGPS = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-
-                // SET MAP LOCATION, markers and default
-                if (checkBoxGPS.isChecked()){
-                    // TODO
-                    LatLng point = null; // new LatLng(PvtFragment.getUserLatitudeDegreesGPS(), PvtFragment.getUserLongitudeDegreesGPS());
-
-                    if (mGPSMarker == null) {
-                        mGPSMarker = mMap.addMarker(new MarkerOptions().position(point));
-                        mGPSMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                    } else {
-                        mGPSMarker.remove();
-                        mGPSMarker = mMap.addMarker(new MarkerOptions().position(point));
-                        mGPSMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                    }
-                } else if (!checkBoxGPS.isChecked() && mGPSMarker != null) {
-                    mGPSMarker.remove();
-                }
-
-                if (checkBoxGAL.isChecked()){
-                    //TODO
-                    LatLng point = null; // new LatLng(PvtFragment.getUserLatitudeDegreesGalileo(),PvtFragment.getUserLongitudeDegreesGalileo());
-
-                    if (mGALMarker == null) {
-                        mGALMarker = mMap.addMarker(new MarkerOptions().position(point));
-                        mGALMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-                    } else {
-                        mGALMarker.remove();
-                        mGALMarker = mMap.addMarker(new MarkerOptions().position(point));
-                        mGALMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-                    }
-                } else if (!checkBoxGAL.isChecked() && mGALMarker != null) {
-                    mGALMarker.remove();
-                }
-
-                mLastKnownLocation = location;
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        }; **/
         
         checkboxLayout = findViewById(R.id.checkboxLayout);
         checkboxLayout.setVisibility(View.VISIBLE);
