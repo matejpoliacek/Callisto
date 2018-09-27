46aae04ea2406e614c21d5c5870df419e4d99855
Matej Poliacek <poliacek.matej@gmail.com>
2018-09-28 01:14:34 +0200

    tweak about page

--- GalileoGame/app/src/main/java/com/chocolateam/galileogame/DescriptionActivity.java
+++ GalileoGame/app/src/main/java/com/chocolateam/galileogame/DescriptionActivity.java
@@ -1,13 +1,27 @@
 package com.chocolateam.galileogame;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DescriptionActivity extends AppCompatActivity {
+
+    View aboutLayout;
+    Drawable aboutBck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
+
+        aboutLayout = findViewById(R.id.aboutLayout);
+        aboutBck = aboutLayout.getBackground();
+        aboutBck.setAlpha(75);
+    }
+
+    public void goBack(View view) {
+        aboutBck.setAlpha(255);
+        finish();
     }
 }
